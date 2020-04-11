5f0f621631fffab9c03f088639eaf63418406eef
Daniel Mora <dani.mora@adsmurai.com>
2020-04-11 17:32:39 +0200

    Add daily info plot

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
 
 import dash
 import dash_core_components as dcc
@@ -10,7 +12,12 @@ from dash.dependencies import (
 )
 
 from geo import geojson_data
-from data import latest_data
+from data import (
+    latest_data,
+    daily_tests,
+    daily_positive_rates,
+    tests_per_abs
+)
 
 import pytz
 import datetime
@@ -23,8 +30,41 @@ source_code_url = 'https://github.com/Dani-Mora/covid-tests-visualization'
 app = dash.Dash()
 server = app.server
 
+last_update_time = datetime.datetime.now()
+current_df = latest_data()
 
-def _map_plot(df) -> go.Figure:
+
+def _daily_info_plot() -> go.Figure:
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    daily_tests_df = daily_tests(current_df)
+    fig.add_trace(go.Scatter(x=daily_tests_df.Data,
+                             y=daily_tests_df.Tests,
+                             mode='markers',
+                             name='Tests'),
+                  secondary_y=False)
+
+    daily_rates_df = daily_positive_rates(current_df)
+    fig.add_trace(go.Scatter(x=daily_rates_df.Data,
+                             y=daily_rates_df['Percentatge positious'],
+                             mode='markers',
+                             name='Positius (%)'),
+                  secondary_y=True)
+
+    fig.update_xaxes(title_text='Data')
+    fig.update_yaxes(title_text='Tests', secondary_y=False)
+    fig.update_yaxes(title_text='Positius (%)',
+                     secondary_y=True)
+
+    fig.update_layout(title='Dades diàries',
+                      autosize=True,
+                      hovermode="x unified",
+                      legend_orientation="h")
+    return fig
+
+
+def _map_plot() -> go.Figure:
+    df = tests_per_abs(current_df)
     fig = go.Figure(go.Choroplethmapbox(geojson=geo_data,
                                         locations=df.ABSCodi,
                                         text=df.ABSDescripcio,
@@ -42,11 +82,12 @@ def _map_plot(df) -> go.Figure:
 
 
 def _last_update_text():
-    cat_time = timezone.localize(datetime.datetime.now())
+    cat_time = timezone.localize(last_update_time)
     cat_str_time = cat_time.strftime('%Y-%m-%d %H:%M:%S')
     return [html.H3(f'Darrera actualització: {cat_str_time}, CEST (UTC+2)')]
 
 
+# Define page structure
 title = html.Div([html.H1('COVID-19 tests realitzats a Catalunya')],
                  style={'textAlign': 'center', 'padding-bottom': '30'})
 
@@ -65,32 +106,39 @@ timer = dcc.Interval(
     n_intervals=0
 )
 
+main_div = html.Div([
+    dcc.Graph(
+        id='main-graph',
+        figure=_map_plot(),
+        style={'width': '100vw', 'height': '60vh'}),
+    dcc.Graph(
+        id='daily-plot-graph',
+        figure=_daily_info_plot(),
+        style={'width': '100vw', 'height': '40vh'})
+])
 
 app.layout = html.Div([
     title,
     last_update,
-    dcc.Graph(id='main-graph',
-              figure=_map_plot(latest_data()),
-              style={'width': '90vw', 'height': '95vh'}),
+    main_div,
     source_code,
     timer
 ])
 
 
-@app.callback(Output(component_id='main-graph', component_property='figure'),
-              [Input(component_id='interval-component',
-                     component_property='n_intervals')])
-def update_main_figure(n):
-    df = latest_data()
-    return _map_plot(df)
-
-
-@app.callback(Output(component_id='last-updated-text',
-                     component_property='children'),
-              [Input(component_id='interval-component',
-                     component_property='n_intervals')])
-def update_main_figure(n):
-    return _last_update_text()
+@app.callback([
+    Output(component_id='daily-plot-graph', component_property='figure'),
+    Output(component_id='main-graph', component_property='figure'),
+    Output(component_id='last-updated-text', component_property='children')],
+    [Input(component_id='interval-component', component_property='n_intervals')]
+)
+def update_app(n):
+    print(f'Update plot: {n}')
+    global last_update_time
+    last_update_time = datetime.datetime.now()
+    global current_df
+    current_df = latest_data()
+    return _daily_info_plot(), _map_plot(), _last_update_text()
 
 
 if __name__ == '__main__':
--- data.py
+++ data.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
+from datetime import datetime
+
 URL = 'https://analisi.transparenciacatalunya.cat/api/views/xuwf-dxjd/rows.csv'
 
 
 def latest_data():
     df = pd.read_csv(URL)
+    df['Data'] = df['Data'].apply(
+        lambda x: datetime.strptime(x, '%d/%m/%Y')
+    )
+    return df
+
+
+def tests_per_abs(df: pd.DataFrame) -> pd.DataFrame:
 
     def extract_stats(df: pd.DataFrame) -> pd.Series:
         return pd.Series({
@@ -17,3 +26,24 @@ def latest_data():
         .groupby(['ABSCodi', 'ABSDescripcio']) \
         .apply(extract_stats) \
         .reset_index()
+
+
+def daily_tests(df: pd.DataFrame) -> pd.DataFrame:
+    return df \
+        .groupby(['Data'])['Data'] \
+        .count() \
+        .reset_index(name='Tests') \
+        .fillna(0)
+
+
+def daily_positive_rates(df: pd.DataFrame) -> pd.DataFrame:
+
+    def _get_positive_perc(df: pd.DataFrame) -> float:
+        positives = df[df['ResultatCovidCodi'] == 1]['NumCasos'].sum()
+        return round(positives / df['NumCasos'].sum() * 100, 2)
+
+    return df \
+        .groupby(['Data']) \
+        .apply(_get_positive_perc) \
+        .reset_index(name='Percentatge positious') \
+        .fillna(0)
