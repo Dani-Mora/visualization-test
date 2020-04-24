cd49ff978eb0367db1ab08d670206c271854a102
Daniel Mora <dani.mora@adsmurai.com>
2020-04-24 21:46:48 +0200

    Remove Null values in data

--- data.py
+++ data.py
@@ -5,12 +5,15 @@ from datetime import datetime
 DATA_COL = 'TipusCasData'
 DIAGNOSE_COL = 'TipusCasDescripcio'
 
+NUM_COL = 'NumCasos'
 POSITIVE_STR = 'Positiu'
 NEGATIVE_STR = 'Sospitós'
 
 
+
 def latest_data():
     df = pd.read_csv('rows.csv')
+    df = df.dropna(subset=[DATA_COL, NUM_COL, DIAGNOSE_COL])
     df[DATA_COL] = df[DATA_COL].apply(
         lambda x: datetime.strptime(x, '%d/%m/%Y')
     )
@@ -21,11 +24,11 @@ def tests_per_abs(df: pd.DataFrame) -> pd.DataFrame:
 
     def extract_stats(df: pd.DataFrame) -> pd.Series:
         return pd.Series({
-            'TotalTests': df['NumCasos'].sum(),
+            'TotalTests': df[NUM_COL].sum(),
             'Positius': df[
-                df[DIAGNOSE_COL] == POSITIVE_STR]['NumCasos'].sum(),
+                df[DIAGNOSE_COL] == POSITIVE_STR][NUM_COL].sum(),
             'Negatius': df[
-                df[DIAGNOSE_COL] == NEGATIVE_STR]['NumCasos'].sum()
+                df[DIAGNOSE_COL] == NEGATIVE_STR][NUM_COL].sum()
         })
 
     return df \
@@ -36,7 +39,7 @@ def tests_per_abs(df: pd.DataFrame) -> pd.DataFrame:
 
 def daily_tests(df: pd.DataFrame) -> pd.DataFrame:
     return df \
-        .groupby([DATA_COL])['NumCasos'] \
+        .groupby([DATA_COL])[NUM_COL] \
         .sum() \
         .reset_index(name='Tests') \
         .fillna(0)
@@ -45,8 +48,8 @@ def daily_tests(df: pd.DataFrame) -> pd.DataFrame:
 def daily_positive_rates(df: pd.DataFrame) -> pd.DataFrame:
 
     def _get_positive_perc(df: pd.DataFrame) -> float:
-        positives = df[df[DIAGNOSE_COL] == POSITIVE_STR]['NumCasos'].sum()
-        return round(positives / df['NumCasos'].sum() * 100, 2)
+        positives = df[df[DIAGNOSE_COL] == POSITIVE_STR][NUM_COL].sum()
+        return round(positives / df[NUM_COL].sum() * 100, 2)
 
     return df \
         .groupby([DATA_COL]) \
